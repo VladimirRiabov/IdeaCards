330a13c26f9fbcbdb32431a980cf711b8797332b
bissir <bissir@mail.ru>
2021-12-04 21:51:15 +0300

    22. Autologin

--- IdeaCards/SceneDelegate.swift
+++ IdeaCards/SceneDelegate.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    //So if he logs in, if he logs out, we will be notified.
+    var authListener: AuthStateDidChangeListenerHandle?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -17,6 +20,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        //мы запускаем эту функцию здесь, т.к эта функция это первое что запуститься в приложении
+        autoLogin()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,7 +52,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
+    
+    //MARK: - Auto Login
+    
+    //функция проверяет входили ли мы в аккаунт на этом устройстве и если входили то направляет на с другой экран
+    func autoLogin() {
+        authListener = Auth.auth().addStateDidChangeListener({ auth, user in
+            //And then we need to provide the name of the listener we want to remove, which is our authentication
+            //So once we are notified that we logged out, we don't want to be notified next time our user logs in
+            Auth.auth().removeStateDidChangeListener(self.authListener!)
+            //We also want to make sure that a user object in our user defaults exist as well.
+            if user != nil && userDefaults.object(forKey: kCURRENTUSER) != nil {
+                //We want to get on a main threat because we are going to change the user interface
+                //So we better be on a main threat because the UI changes happening.
+                DispatchQueue.main.async {
+                    //So we have a user and the current user saved on user defaults. Please go to the application and go to up function.
+                    self.goToApp()
+                }
+                
+            }
+        })
+    }
+    private func goToApp() {
+        //OK, so we have initialized these Tabbar controller.
+        let mainView = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainView") as! UITabBarController
+        //And what we need to do now is to set this as the root view of our application.
+        // var window: UIWindow? наверху отвечает за первое и главное окно
+        self.window?.rootViewController = mainView
+        
+    }
 }
 
